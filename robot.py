6ab82e1d896ab126edb3071323a95600a489c984
mathias <mathias.wiesbauer@gmail.com>
2016-10-28 22:42:20 -0400

    calibrating right motor

--- robot.py
+++ robot.py
@@ -10,18 +10,18 @@ def stopRobot():
 
 def setupLeftMotor():
     # INITIALIZE LEFT MOTOR
-    leftMotor = Motor(33, 35, 37, 18, "left")     ## SETS THE GPIO PINS
+    leftMotor = Motor(33, 35, 37, 18)     ## SETS THE GPIO PINS
     #leftPWM = leftMotor.backward(255, 30)
     return leftMotor
 
 
 def setupRightMotor():
-    rightMotor = Motor(40, 38, 36, 16, "right")    ## SETS THE GPIO PINS
+    rightMotor = Motor(40, 38, 36, 16)    ## SETS THE GPIO PINS
     #rightPWM = rightMotor.backward(255, 30)
     return rightMotor
 
 
-def startRobot(sleeptime, pwm, intensity, wheel):
+def startRobot(sleeptime, pwm, intensity):
 
 
     #if wheel == "left":
@@ -66,10 +66,9 @@ if __name__ == ("__main__"):
     parser.add_argument("sleep", help="sleep time in seconds")
     parser.add_argument("intensity", help="value from 0 to 100")
     parser.add_argument("pwm", help="pwm value from 0 to 255")
-    parser.add_argument("wheel", help="[left, right]")
     args = parser.parse_args()
 
 
 
 
-    startRobot(float(args.sleep), float(args.pwm), float(args.intensity), args.wheel)
+    startRobot(float(args.sleep), float(args.pwm), float(args.intensity))
