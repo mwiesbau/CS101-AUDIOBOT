f0892cc39af22b87bbce4ff775b1148bc19901a6
tomachup <toma_chup@yahoo.com>
2016-11-20 17:09:55 -0500

    timer added

--- audiocontrol.py
+++ audiocontrol.py
@@ -18,20 +18,21 @@ percentSpeed = 0
 highestSpeed = 0
 maxSpeed = 140.0 - threshold
 adjustedDecibel = 0.0
+seconds = 300
 
 while True:
     counter += 1
     data = np.fromstring(stream.read(2**10),dtype=np.int16)
     rms = audioop.rms(data,2)
     
-
+    seconds = seconds-1
     # WEIRD ISSUE ON SOME WINDOWS PC's 
     if rms == 0:
         continue
 
     # SEND THE DB READING TO TIPBOARD ########################################################
     decibel = 20 * math.log10(rms)
-    publishToTipboard.update_just_value_config("rednoise", "#FF9618")
+    publishToTipboard.update_just_value_config("orangenoise", "#FF9618")
     publishToTipboard.update_just_value_config("bluenoise", "blue")
 
     #Take average of decibels #######################################################
@@ -40,7 +41,7 @@ while True:
     avgDecibel = decibelSum / avgCount
 
     # SEND AVG DB ################################################################
-    publishToTipboard.update_just_value_config("redavg", "#FF9618")
+    publishToTipboard.update_just_value_config("orangeavg", "#FF9618")
     publishToTipboard.update_just_value_config("blueavg", "blue")
 
     #I figured the car shouldnt move when the room is fairly quiet so I set it up so speed is 0
@@ -53,27 +54,31 @@ while True:
         highestSpeed = percentSpeed
 
     # SEND SPEED(big_data) ################################################################
-    publishToTipboard.update_big_value_config("redbigvalue", "#FF9618")
+    publishToTipboard.update_big_value_config("orangebigvalue", "#FF9618")
     publishToTipboard.update_big_value_config("bluebigvalue", "blue")
 
-
-
     #publish results to tipboard#######################################################
-    publishToTipboard.update_big_value("redbigvalue", "", "Current Speed", \
+
+    #ORANGE TURN#########################
+    publishToTipboard.update_big_value("orangebigvalue", "", "Current Speed", \
                                        "{:3.2f}%".format(percentSpeed), "Distance", \
                                        decibelSum/200, "Max Speed", \
                                        int(highestSpeed))
+    publishToTipboard.update_just_value("orangeavg", "Average Level", "", str(round(avgDecibel,0)))
+    publishToTipboard.update_just_value("orangenoise", "Current Level", "", str(round(decibel,0)))
+
+    #BLUE TURN########################
     publishToTipboard.update_big_value("bluebigvalue", "", "Current Speed", \
                                        "{:3.2f}%".format(percentSpeed), "Distance", \
                                        decibelSum/200, "Max Speed", \
                                        int(highestSpeed))
-    publishToTipboard.update_just_value("redavg", "Average Level", "", str(round(avgDecibel,0)))
     publishToTipboard.update_just_value("blueavg", "Average Level", "", str(round(avgDecibel,0)))
-    publishToTipboard.update_just_value("rednoise", "Current Level", "", str(round(decibel,0)))
     publishToTipboard.update_just_value("bluenoise", "Current Level", "", str(round(decibel,0)))
+    
+    #COMBINED MEASUREMENTS############
     publishToTipboard.update_pie_chart("maxspeedpiechart", "Max Speed", "blue", "orange", int(highestSpeed), avgDecibel)
-    publishToTipboard.update_pie_chart("distancepiechart", "Distance", "blue", "orange", \
-                                       avgDecibel, int(highestSpeed))
+    publishToTipboard.update_just_value_config("timer", "black")
+    publishToTipboard.update_just_value("timer", "Timer", "", (int)(seconds/10))
         
     #########################################################################################
 
